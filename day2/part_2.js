06c45c2f4953dd0165020f69a43ee7c5e75bdaba
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-03-22 17:01:10 +0100

    Advent of code Day two

--- day2/part_2.js
+++ day2/part_2.js
@@ -1,26 +1,31 @@
 const fs = require("fs");
-let input = fs.readFileSync("./demo.txt").toString();
+let input = fs.readFileSync("./input.txt").toString();
 const elem_riga = input.split("\n");
 
 let horizontal = 0;
-let depth = 0;
 let sum = 0;
 let aim = 0;
+let depth = 0;
+let depth_saver = 0;
+
+// USO QUESTO PER RICAVARMI IL PRIMO NUMERO CHE ESCE IN UNA STRINGA
+//elem_riga[i].match(/(\d+)/)[1]
 
 for (let i = 0; i < elem_riga.length; i++) {
-  if (elem_riga[i].includes("forward")) {
-    horizontal += +elem_riga[i].match(/(\d+)/)[1] + aim;
-    depth = +elem_riga[i].match(/(\d+)/)[1] * aim;
+  if (+elem_riga[i].includes("forward") && aim === 0) {
+    horizontal = +elem_riga[i].match(/(\d+)/)[1];
   }
-  if (elem_riga[i].includes("down")) {
-    depth += +elem_riga[i].match(/(\d+)/)[1];
+
+  if (+elem_riga[i].includes("forward") && aim >= 1) {
+    horizontal += +elem_riga[i].match(/(\d+)/)[1];
+    depth = +elem_riga[i].match(/(\d+)/)[1] * aim;
+    depth_saver += depth;
+  }
+  if (+elem_riga[i].includes("down")) {
     aim += +elem_riga[i].match(/(\d+)/)[1];
   }
-  if (elem_riga[i].includes("up")) {
-    depth -= +elem_riga[i].match(/(\d+)/)[1];
+  if (+elem_riga[i].includes("up")) {
     aim -= +elem_riga[i].match(/(\d+)/)[1];
   }
 }
-
-sum = horizontal * depth;
-console.log(sum);
+console.log(depth_saver * horizontal); //2044620088
