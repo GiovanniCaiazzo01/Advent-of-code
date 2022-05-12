590848a3cd22e27f23968bfe2a96a252d40930ea
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-05-12 22:00:16 +0200

    help

--- day3/part_2.js
+++ day3/part_2.js
@@ -8,18 +8,32 @@ let tmp = [];
 let c = [];
 
 const posizione = (j, i) => {
-  if (j.charAt([i]).includes(1)) return uno.push(j);
-  if (j.charAt([i]).includes(0)) return zero.push(j);
+  if (j.charAt([i]) == 1) return uno.push(j);
+  if (j.charAt([i]) == 0) return zero.push(j);
 };
+
+// const ocaz = (array, i) => {
+//   if (array[i].charAt([i]) == 1) {
+//     uno = [];
+//     uno.push(array);
+//   }
+//   if (array[i].charAt([i]) == 0) {
+//     zero = [];
+//     zero.push(array);
+//   }
+// };
 
 const oxygen_generator_rating = (elem_riga) => {
   for (let i = 0; i < elem_riga[0].length; i++) {
     for (j of elem_riga) {
       posizione(j, i);
     }
-    uno.length > zero.length
-      ? console.log(`ARRAY DI UNO \n${uno}`)
-      : console.log(`ARRAY DI ZERO \n ${zero}`);
+    if (uno.length > zero.length) {
+      posizione()
+    }
+    console.log(
+      `sto stampando l'array UNO ${uno} \n sto stampando l'array zero ${zero}`
+    );
     zero = [];
     uno = [];
   }
