53754ae2664fb3ebd3d35fd289d4b38ed01bc7cc
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-07-13 17:02:36 +0200

    d

--- day3/test.js
+++ day3/test.js
@@ -1,20 +1,47 @@
 const fs = require("fs");
+const { setFlagsFromString } = require("v8");
 const input = fs.readFileSync("./demo.txt").toString();
 const elem_riga = input.split("\n");
 
 const uno = [];
 const zero = [];
-const oxygen_generator_rating = (elem_riga, index, index_due) => {
+let tmp = [];
+
+let sinistra = 0;
+let destra = 1;
+
+const filtroStonks = (uno, zero, index) => {
+  if (uno.lenght > zero.lenght) {
+    return uno.filter((f) => {
+      f.substring(sinistra, destra) == 1
+        ? Promise.resolve((uno = [])).then((e) => e.push(f))
+        : Promise.resolve((zero = [])).then((e) => e.push(f));
+    });
+  }
+};
+
+const oxygen_generator_rating = (elem_riga, index) => {
   for (elemX of elem_riga[index++]) {
-    for (elemY of elem_riga[index_due++]) {
-      console.log(elemY);
-      // elemY.filter((e) => (e == "1" ? uno.push(e) : zero.push(e)));
+    for (elemY of elem_riga) {
+      tmp.push(elemY);
     }
+
+    console.log("AAAAAAAAAAA");
+    tmp.filter((f) =>
+      f.substring(sinistra, destra) == 1 ? uno.push(f) : zero.push(f)
+    );
+
+    filtroStonks(uno, zero, index);
+    console.log(pisello);
+    console.log(uno);
+    console.log(zero);
+    sinistra++;
+    destra++;
   }
 };
 
 console.log(uno);
 const co2_scrubber_rating = (elem_riga) => {};
 
-const ogr = oxygen_generator_rating(elem_riga, (index = 0), (index_due = 0));
+const ogr = oxygen_generator_rating(elem_riga, (index = 0));
 const co2 = co2_scrubber_rating(elem_riga);
