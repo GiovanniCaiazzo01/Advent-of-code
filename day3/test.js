c3aea243129fa22ef97bcab059643d2b72301ccf
GiovanniCaiazzo01 <giovannicaiazzo@gmail.com>
2022-07-26 22:38:56 +0200

    refactor

--- day3/test.js
+++ day3/test.js
@@ -1,38 +1,19 @@
 const fs = require("fs");
-const { setFlagsFromString } = require("v8");
-const input = fs.readFileSync("./demo.txt").toString();
-const elem_riga = input.split("\n");
+const input = fs.readFileSync("./demo.txt").toString().split("\n");
 
 const uno = [];
 const zero = [];
 let tmp = [];
 
-let sinistra = 0;
 let destra = 1;
+let sinistra = 0;
 
-const filtroStonks = (uno, zero) => {};
-
-const oxygen_generator_rating = (elem_riga, index) => {
-  for (elemX of elem_riga[index++]) {
-    for (elemY of elem_riga) {
-      tmp.push(elemY);
-    }
-
-    console.log("AAAAAAAAAAA");
-    tmp.filter((f) =>
-      f.substring(sinistra, destra) == 1 ? uno.push(f) : zero.push(f)
-    );
-    tmp = [];
-    filtroStonks(uno, zero);
-    console.log(uno);
-    console.log(zero);
-    sinistra++;
-    destra++;
+for (let i = 0; i < input[0].length; i++) {
+  for (let j = 0; j < input.length; j++) {
+    const data = input[j].substring(sinistra, destra);
+    tmp.push(data);
   }
-};
-
-console.log(uno);
-const co2_scrubber_rating = (elem_riga) => {};
-
-const ogr = oxygen_generator_rating(elem_riga, (index = 0));
-const co2 = co2_scrubber_rating(elem_riga);
+  sinistra++;
+  destra++;
+  console.log("//////////////////////////////////////");
+}
