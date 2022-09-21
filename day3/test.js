0bef490cc14d59e0b65e6212aecc60206df9f93d
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-09-21 21:39:24 +0200

    update day3

--- day3/test.js
+++ day3/test.js
@@ -4,33 +4,36 @@ const input = fs.readFileSync("./demo.txt").toString().split("\n");
 let array = [];
 let uno = [];
 let zero = [];
-let index = 0;
+
 // capire perchè vince sempre 0 dopo la prima iterazione
-let oxygen_generator_rating = () => {
+let oxygen_generator_rating = (index, second_index) => {
   for (const x of input[0]) {
     for (const y of input) {
-      if (array.length < 1) {
+      if (index < 1) {
         y[index] == "1" ? uno.push(y) : zero.push(y);
-      } else if (array.length > 1) {
-        array[index] == "1" ? uno.push(y) : zero.push(y);
+      } else if (index >= 1) {
+        array[0][second_index].substring(index, index + 1) == "1"
+          ? uno.push(array[0][second_index])
+          : zero.push(array[0][second_index]);
+        // console.log("ARRAY DI INDEX = ", index);
       }
+      second_index++;
     }
 
-    console.log(
-      `interazione numero ${index} il vincitore di questa iterazione è ${
-        uno.length > zero.length
-          ? console.log("E' uno", uno)
-          : console.log("E' zero", zero)
-      }`
-    );
+    second_index = 0;
+    index < 1 &&
+      console.log(`array UNO: => [${uno}]    array ZERO: => [${zero}]`);
+
+    index >= 1 &&
+      console.log(`array UNO: => [${uno}]    array ZERO: => [${zero}]`);
+
     index++;
     array = [];
 
-    uno.length > zero.length ? array.push(...uno) : array.push(...zero);
-    uno.length > zero.length ? console.log("vince 1") : console.log("vince 0");
-
+    uno.length > zero.length ? array.push(uno) : array.push(zero);
+    console.log(`array VINCENTE ${array}`);
     uno = [];
     zero = [];
   }
 };
-oxygen_generator_rating();
+oxygen_generator_rating((index = 0), (second_index = 0));
