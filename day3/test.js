84e4d4569688239fd549dd26d6bdb29053874eb4
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-09-22 21:43:27 +0200

    finito capire ultima cosa

--- day3/test.js
+++ day3/test.js
@@ -5,13 +5,13 @@ let array = [];
 let uno = [];
 let zero = [];
 
-// capire perchè vince sempre 0 dopo la prima iterazione
+// TODO: CAPIRE PERCHE IL NEGATIVO NON FUNZIONA'
 let oxygen_generator_rating = (index, second_index) => {
   for (const x of input[0]) {
     for (const y of input) {
       if (index < 1) {
         y[index] == "1" ? uno.push(y) : zero.push(y);
-      } else if (index >= 1) {
+      } else if (index >= 1 && second_index < array[0].length) {
         array[0][second_index].substring(index, index + 1) == "1"
           ? uno.push(array[0][second_index])
           : zero.push(array[0][second_index]);
@@ -30,10 +30,65 @@ let oxygen_generator_rating = (index, second_index) => {
     index++;
     array = [];
 
-    uno.length > zero.length ? array.push(uno) : array.push(zero);
+    uno.length > zero.length || uno.length === zero.length
+      ? array.push(uno)
+      : array.push(zero);
+
     console.log(`array VINCENTE ${array}`);
     uno = [];
     zero = [];
   }
+  return +array;
 };
-oxygen_generator_rating((index = 0), (second_index = 0));
+
+const co2_scrubber_rating = (index, second_index, array, uno, zero) => {
+  for (const x of input[0]) {
+    for (const y of input) {
+      if (index < 1) {
+        y[index] == "0" ? zero.push(y) : uno.push(y);
+      } else if (index >= 1 && second_index < array[0].length) {
+        array[0][second_index].substring(index, index + 1) == "0"
+          ? zero.push(array[0][second_index])
+          : uno.push(array[0][second_index]);
+        // console.log("ARRAY DI INDEX = ", index);
+      }
+      second_index++;
+    }
+
+    second_index = 0;
+    index < 1 &&
+      console.log(`array UNO: => [${uno}]    array ZERO: => [${zero}]`);
+
+    index >= 1 &&
+      console.log(`array UNO: => [${uno}]    array ZERO: => [${zero}]`);
+
+    index++;
+    array = [];
+
+    zero.length > uno.length || zero.length === uno.length
+      ? array.push(zero)
+      : array.push(uno);
+
+    console.log(`array VINCENTE ${array}`);
+    uno = [];
+    zero = [];
+  }
+  return +array;
+};
+
+const ogr = oxygen_generator_rating(
+  (index = 0),
+  (second_index = 0),
+  (array = []),
+  (uno = []),
+  (zero = [])
+);
+const co2 = oxygen_generator_rating(
+  (index = 0),
+  (second_index = 0),
+  (array = []),
+  (uno = []),
+  (zero = [])
+);
+
+console.log(ogr, co2);
