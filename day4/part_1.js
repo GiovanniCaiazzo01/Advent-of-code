7ce81f29e49f18dfd0b1a5e6aa992c82cba99c41
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-11-30 16:47:27 +0100

    update

--- day4/part_1.js
+++ day4/part_1.js
@@ -2,18 +2,27 @@ const fs = require("fs");
 const input = fs.readFileSync("./demo.txt").toString();
 const elem_riga = input.split("\n");
 
-console.log(elem_riga);
 const number = [
   7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18,
   20, 8, 19, 3, 26, 1,
 ];
 
-for (x in elem_riga) {
-  console.log("sono sopra");
-  console.log(elem_riga[x]);
-  for (y in elem_riga[0]) {
-    console.log("sono sotto");
-
-    console.log(elem_riga[0]);
+const checkForBingo = (element_to_send) => {
+  const have_number = [];
+  if (element_to_send === "") {
+    return;
   }
+
+  let counter = 0;
+  for (const n of number) {
+    have_number.push(n);
+    for (const single_number of have_number) {
+      if (have_number[counter] === n) {
+      }
+    }
+  }
+};
+
+for (const el of elem_riga) {
+  checkForBingo(el);
 }
