2c241735658d35ea1e358830e2f134f1c4385bef
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-12-01 21:34:12 +0100

    part-2

--- 2022/day-1/part_2.js
+++ 2022/day-1/part_2.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const file = fs.readFileSync("demo.txt", { encoding: "utf-8" });
+const file = fs.readFileSync("input.txt", { encoding: "utf-8" });
 const input = file.split("\n");
 
 const retrive_elves_calories = (input) => {
@@ -15,17 +15,35 @@ const retrive_elves_calories = (input) => {
       sum = 0;
     }
   }
-  console.log(calories);
+  return calories;
 };
 
 const gang_of_three = (elves_calories) => {
-  const ordered_calories = [];
+  let top_calories = [];
+  for (let i = 0; i < elves_calories.length; i++) {
+    for (let j = 0; j < elves_calories.length - i - 1; j++) {
+      if (elves_calories[j + 1] < elves_calories[j]) {
+        [elves_calories[j + 1], elves_calories[j]] = [
+          elves_calories[j],
+          elves_calories[j + 1],
+        ];
+      }
+    }
+  }
+  top_calories = [
+    elves_calories[elves_calories.length - 1],
+    elves_calories[elves_calories.length - 2],
+    elves_calories[elves_calories.length - 3],
+  ];
+  return top_calories;
 };
 
 const start = (input) => {
   const elves_calories = retrive_elves_calories(input);
   const top_three = gang_of_three(elves_calories);
+  const result = top_three.reduce((prev, curr) => prev + curr);
+  return result;
 };
 
 const result = start(input);
-// console.log(result);
+console.log("PART 2 RESULT => ", result);
