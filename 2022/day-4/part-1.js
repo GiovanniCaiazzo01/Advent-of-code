e898dd4e5eb3164168f211855eb2074729294853
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-12-05 13:19:26 +0100

    day 4 part 1 completed

---
+++ 2022/day-4/part-1.js
@@ -0,0 +1,30 @@
+const input = require("fs")
+  .readFileSync("input.txt", { encoding: "utf-8" })
+  .split("," || "\n")
+  .join(",")
+  .split("\n")
+  .join("," || "\n")
+  .split(",")
+  .map((el) => el.split("-"));
+let arr = [];
+let count = 0;
+let sum = 0;
+const check = (left, right) => {
+  if (
+    (+left[0] <= +right[0] && +left[1] >= +right[1]) ||
+    (+right[0] <= +left[0] && +right[1] >= +left[1])
+  ) {
+    sum += 1;
+  }
+};
+for (const el of input) {
+  arr.push(el);
+  count++;
+  if (count === 2) {
+    check(arr[0], arr[1]);
+    arr = [];
+    count = 0;
+  }
+}
+
+console.log(sum);
