f870c81358151f6de2fd0b7e1d28798a33ae7025
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-12-05 21:01:29 +0100

    day 4 completed

--- 2022/day-4/part-2.js
+++ 2022/day-4/part-2.js
@@ -1,5 +1,5 @@
 const input = require("fs")
-  .readFileSync("demo.txt", { encoding: "utf-8" })
+  .readFileSync("input.txt", { encoding: "utf-8" })
   .split("," || "\n")
   .join(",")
   .split("\n")
@@ -11,12 +11,25 @@ let count = 0;
 let sum = 0;
 const check = (left, right) => {
   // controllo  se sono uguali  ad x
-
-  if (
-    (+left[0] <= +right[0] && +left[1] >= +right[1]) ||
-    (+right[0] <= +left[0] && +right[1] >= +left[1])
-  ) {
-    sum += 1;
+  const left_limit = +left[1];
+  const right_limit = +right[1];
+  let left_min = Math.min(...left);
+  let right_min = Math.min(...right);
+  const left_to_compare = [];
+  const right_to_compare = [];
+  console.log(left_min, right_min);
+  for (let i = left_min; i <= left_limit; i++) {
+    left_to_compare.push(i);
+  }
+  for (let i = right_min; i <= right_limit; i++) {
+    right_to_compare.push(i);
+  }
+  for (let l of left_to_compare) {
+    for (let r of right_to_compare) {
+      if (left_to_compare.includes(r)) {
+        return sum++;
+      }
+    }
   }
 };
 for (const el of input) {
@@ -24,9 +37,9 @@ for (const el of input) {
   count++;
   if (count === 2) {
     check(arr[0], arr[1]);
+
     arr = [];
     count = 0;
   }
 }
-
 console.log(sum);
