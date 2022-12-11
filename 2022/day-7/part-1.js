751a579ecfde6bda3abd939a7ed744351b750c8e
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-12-11 14:50:28 +0100

    avanzamento giorno 7

--- 2022/day-7/part-1.js
+++ 2022/day-7/part-1.js
@@ -2,32 +2,63 @@ const input = require("fs")
   .readFileSync("demo.txt", { encoding: "utf-8" })
   .split("\n");
 
-const obj = {
-  comand: [],
-  dir: {},
+const dit_three_structure = {};
+
+const format_element = (element) => {
+  if (element.includes("$ cd")) {
+    return element.replace("$ cd", "").trim();
+  }
 };
 
-const retrive_command = (input) => {
-  for (const el of input) {
-    if (el.includes("$") && !el.includes("ls")) {
-      obj.comand.push(el.replace("$", "").trim());
+const retrive_value = (number) => {
+  if (+number < 100000) {
+    return +number;
+  }
+};
+
+const riempi_dir = (index, input, element) => {
+  const value = [];
+  for (let i = index; i < input.length; i++) {
+    if (input[i]?.includes("$ cd")) {
+      break;
+    }
+    if (element in dit_three_structure) {
+      if (input[i].includes("dir")) {
+        const dir = input[i].substring(4, 5).trim();
+        console.log(dir);
+        value.push(
+          (dit_three_structure[dir] = {
+            [dir]: {},
+          })
+        );
+      } else {
+        const file = input[i].replace(/[^0-9]+/g, "");
+        if (+file < 100000) {
+          value.push(file);
+        }
+      }
     }
   }
+  return (dit_three_structure[element] = value);
 };
-const retrive_single_dir = () => {
-  for (const el of obj.comand) {
-    obj.dir[el.replace(`${"cd" || "ls"}`, "").trim()] = "";
-  }
-};
-retrive_command(input);
-retrive_single_dir();
 
-for (const el of obj.comand) {
-  const to_stamp = el.substring(0, 1);
-  log;
+let formatted_element;
+for (let i = 0; i < input.length; i++) {
+  const current_el = input[i];
+  const prev_is_ls = input[i - 1]?.includes("$ ls") ? input[i - 1] : null;
+  if (current_el.includes("cd")) {
+    formatted_element = format_element(current_el);
+    if (!dit_three_structure[formatted_element]) {
+      dit_three_structure[formatted_element] = "";
+    }
+  }
+  if (prev_is_ls) {
+    const get_index = input.indexOf(current_el);
+    riempi_dir(get_index, input, formatted_element);
+  }
 }
 
-console.log(obj);
+console.log(dit_three_structure, "\n--------------------------------");
 [
   "$ cd /",
   "$ ls",
@@ -35,17 +66,20 @@ console.log(obj);
   "14848514 b.txt",
   "8504156 c.dat",
   "dir d",
+
   "$ cd a",
   "$ ls",
   "dir e",
   "29116 f",
   "2557 g",
   "62596 h.lst",
+
   "$ cd e",
   "$ ls",
   "584 i",
   "$ cd ..",
   "$ cd ..",
+
   "$ cd d",
   "$ ls",
   "4060174 j",
@@ -53,3 +87,26 @@ console.log(obj);
   "5626152 d.ext",
   "7214296 k",
 ];
+
+// const retrive_command = (input) => {
+//   for (const el of input) {
+//     if (el.includes("$") && !el.includes("ls")) {
+//       obj.comand.push(el.replace("$", "").trim());
+//     }
+//   }
+// };
+// const retrive_single_dir = () => {
+//   for (const el of obj.comand) {
+//     obj.dir[el.replace(`${"cd" || "ls"}`, "").trim()] = "";
+//   }
+// };
+// retrive_command(input);
+// retrive_single_dir();
+
+// for (const el of obj.comand) {
+//   const dir = el.substring(3, 4);
+//   console.log(dir);
+//   if(el !== "."){
+//     obj.dir_three[dir] =
+//   }
+// }
