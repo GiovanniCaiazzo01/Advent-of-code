0d60a3fd8f4e202e37057395e1862d77558eedb6
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-12-14 23:01:06 +0100

    small changes

--- 2022/day-7/part-1.js
+++ 2022/day-7/part-1.js
@@ -51,33 +51,25 @@ for (let i = 0; i < input.length; i++) {
   }
 }
 
-console.log(dit_three_structure);
 let cont = 0;
 for (const dir in dit_three_structure) {
   cont = 0;
   let prev = dit_three_structure[dir];
-  // console.log(prev);
   for (const el in dit_three_structure) {
     let curr = el;
-    console.log(curr);
     if (prev[cont] === undefined) continue;
     if (prev[cont][el] === undefined) continue;
-    // devi continuare da qui, se l'elemento corrente  si trova nelle cheivi dell'oggetto prev
-    // e l'elemento prev è un oggetto allora esegui le istruzioni
+
     if (curr in prev[cont]) {
       prev[cont][el] = dit_three_structure[curr];
-      console.log(dit_three_structure[el]);
+      delete dit_three_structure[curr];
     }
     cont++;
   }
 }
-console.log();
-console.log(
-  dit_three_structure,
-  "weeeeeeeeeeeeeeeeeeeee",
-  dit_three_structure["/"][0].a[0].e,
-  "\n--------------------------------"
-);
+
+// adesso devi semplicemente iterare lungi l'oggetto è sommare tutti i numeri
+// { '/': [ { a: [Array] }, { d: [] } ], e: [ '584' ], '..': '' }
 [
   "$ cd /",
   "$ ls",
