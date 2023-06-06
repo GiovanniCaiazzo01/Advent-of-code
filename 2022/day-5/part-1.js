153258981d9f8eb330de59bbd6fddcc5accaab51
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-06-06 20:13:17 +0200

    wip: solutions

--- 2022/day-5/part-1.js
+++ 2022/day-5/part-1.js
@@ -1,78 +1,48 @@
+// const input = require("fs")
+//   .readFileSync("demo.txt", { encoding: "utf-8" })
+//   .split("\n")
+//   .map((el) => el.replace(/[\[\]']+/g, ""))
+//   .map((el) => el.replace(/\d+/g, ""))
+//   .map((el) => el.replace(/[a-z]/g, ""));
+
 const input = require("fs")
   .readFileSync("demo.txt", { encoding: "utf-8" })
-  .split("\n")
-  .map((el) => el.replace(/[\[\]']+/g, ""))
-  .map((el) => el.replace(/\d+/g, ""))
-  .map((el) => el.replace(/[a-z]/g, ""));
+  .split("\n");
 
-const divide_space = (input) => {
-  let counter = 0;
-  const to_return = [];
-  let tmp = [];
-  for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input[i].length; j++) {
-      if (input[i][j] === " ") {
-        counter++;
-      }
-      if (counter === 4 && input[i][j] !== " ") {
-        tmp.push(" ");
-      }
-      if (input[i][j] !== " ") {
-        tmp.push(input[i][j]);
-      } else if (counter === 8 && input[i][j - 4] !== " ") {
-        tmp.push(" ");
-      }
+let pre = 0;
+let next = 1;
+let spaces = 0;
+const new_map = [];
+
+for (let i = 0; i < input.length; i++) {
+  pre = 0;
+  next = 1;
+  for (let j = 0; j < input.length; j++) {
+    if (!new_map[i]) new_map[i] = [];
+    if (spaces === 4) {
+      new_map[i].push(" ");
     }
-    to_return.push(tmp);
-    tmp = [];
-    counter = 0;
-  }
-
-  return to_return;
-};
-
-const fix_raw = (raw) => {
-  for (const container of raw) {
-    for (const el of container) {
-      console.log(el);
+    if (input[i].substring(pre, next) === " ") {
+      spaces++;
+      pre++;
+      next++;
+    }
+    if (input[i].substring(pre, next) === "[") {
+      spaces = 0;
+      new_map[i].push(input[i].substring(pre + 1, next + 1));
     }
   }
-};
+}
 
-const raw = divide_space(input);
-console.log(raw);
-
-// prima di arrivare a questo bisogna elkiminare tutti i restanti elementi che non ci servono
-const formatted_input = fix_raw(raw);
-
+console.log(new_map);
 // [
-//   [ ' ', 'D', ' ' ],
-//   [ 'N', 'C' ],
-//   [ 'Z', 'M', 'P' ],
-//   [ '1', ' ', '2', '3' ],
-//   [],
-//   [
-//     'm', 'o', 'v', 'e',
-//     '1', 'f', 'r', 'o',
-//     'm', '2', ' ', 't',
-//     ' ', 'o', '1'
-//   ],
-//   [
-//     'm', 'o', 'v', 'e',
-//     '3', 'f', 'r', 'o',
-//     'm', '1', ' ', 't',
-//     ' ', 'o', '3'
-//   ],
-//   [
-//     'm', 'o', 'v', 'e',
-//     '2', 'f', 'r', 'o',
-//     'm', '2', ' ', 't',
-//     ' ', 'o', '1'
-//   ],
-//   [
-//     'm', 'o', 'v', 'e',
-//     '1', 'f', 'r', 'o',
-//     'm', '1', ' ', 't',
-//     ' ', 'o', '2'
-//   ]
+//   "    [D]    ",
+//   "[N] [C]    ",
+//   "[Z] [M] [P]",
+//   " 1   2   3 ",
+//   "",
+//   "move 1 from 2 to 1",
+//   "move 3 from 1 to 3",
+//   "move 2 from 2 to 1",
+//   "move 1 from 1 to 2",
 // ]
